e6ec9e5fd1b7367b2ae5f7b641a2633744772d50
quan <nnmhqn2003@gmail.com>
2024-04-27 10:20:35 +0700

    eq

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '/node_modules/bootstrap/dist/js/bootstrap.min.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AddRoom from './components/room/AddRoom'
 import ExistingRooms from './components/room/ExistingRooms'
 import Home from './components/home/Home'
@@ -21,36 +21,36 @@ import AuthProvider from './components/auth/AuthProvider'
 import RequireAuth from './components/auth/RequireAuth'
 import Logout from './components/auth/Logout'
 function App() {
-  return(
-    <AuthProvider>
-      <main>
-        <Router>
-          <NavBar/>
-          <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/add-room' element={<AddRoom/>}></Route>
-            <Route path='/existing-rooms' element={<ExistingRooms/>}></Route>
-            <Route path='/book-room/:roomId' element={
-              <RequireAuth>
-                <Checkout/>
-              </RequireAuth>
-            }></Route>
-            <Route path='/edit-room/:roomId' element={<EditRoom/>}></Route>
-            <Route path='/browse-all-rooms' element={<RoomListing/>}></Route>
-            <Route path='/admin' element={<Admin/>}></Route>
-            <Route path='/booking-success' element={<BookingSuccess/>}></Route>
-            <Route path='/existing-bookings' element={<Bookings/>}></Route>
-            <Route path='/find-booking' element={<FindBooking/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/register' element={<Registration/>}></Route>
-            <Route path='/profile' element={<Profile/>}></Route>
-            <Route path='/logout' element={<Logout/>}></Route>
-          </Routes>
-        </Router>
-        <Footer/>
-      </main>
-    </AuthProvider>
-  )
+    return (
+        <AuthProvider>
+            <main>
+                <Router>
+                    <NavBar/>
+                    <Routes>
+                        <Route path='/' element={<Home/>}></Route>
+                        <Route path='/add-room' element={<AddRoom/>}></Route>
+                        <Route path='/existing-rooms' element={<ExistingRooms/>}></Route>
+                        <Route path='/book-room/:roomId' element={
+                            <RequireAuth>
+                                <Checkout/>
+                            </RequireAuth>
+                        }></Route>
+                        <Route path='/edit-room/:roomId' element={<EditRoom/>}></Route>
+                        <Route path='/browse-all-rooms' element={<RoomListing/>}></Route>
+                        <Route path='/admin' element={<Admin/>}></Route>
+                        <Route path='/booking-success' element={<BookingSuccess/>}></Route>
+                        <Route path='/existing-bookings' element={<Bookings/>}></Route>
+                        <Route path='/find-booking' element={<FindBooking/>}></Route>
+                        <Route path='/login' element={<Login/>}></Route>
+                        <Route path='/register' element={<Registration/>}></Route>
+                        <Route path='/profile' element={<Profile/>}></Route>
+                        <Route path='/logout' element={<Logout/>}></Route>
+                    </Routes>
+                </Router>
+                <Footer/>
+            </main>
+        </AuthProvider>
+    )
 }
 
 export default App
